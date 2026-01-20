4ed06d656627185e41cac2893a8c0e1223e2121a
gcosta295 <156736652+gcosta295@users.noreply.github.com>
2026-01-19 20:17:34 -0400

    prueba de primer commit
    a

--- src/main/java/com/mycompany/sisoperativos/costa/tucci/SisOperativosCostaTucci.java
+++ src/main/java/com/mycompany/sisoperativos/costa/tucci/SisOperativosCostaTucci.java
@@ -11,6 +11,6 @@ package com.mycompany.sisoperativos.costa.tucci;
 public class SisOperativosCostaTucci {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!");
     }
 }
