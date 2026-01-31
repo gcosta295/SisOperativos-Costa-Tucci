3a72d96d0c25f70ff32d5efca6e0370e383d1816
astv06 <156257557+astv06@users.noreply.github.com>
2026-01-31 17:29:37 -0500

    periodico y aperiodico
    creacion de los procesos periodicos y aperiodicos, ademas de ajustes en estructura de pcb y process

--- src/main/java/com/mycompany/sisoperativos/Process.java
+++ src/main/java/com/mycompany/sisoperativos/Process.java
@@ -64,4 +64,94 @@ public class Process {
         this.processName=null;
     }
     
+    public Process periodicProcess(PCB pcb){
+        int id = pcb.getPriority();
+        boolean flag = true;
+        if ((id % 13 == 0)&&(flag)){
+           flag = false;
+           pcb.setPriority(1);
+           this.processName="Image upload";
+        }
+        if ((id % 11 == 0)&&(flag)){
+            flag = false;
+           pcb.setPriority(2);
+           this.processName="Temperature";
+        }
+        if ((id % 7 == 0)&&(flag)){
+            flag = false;
+           pcb.setPriority(3);
+           this.processName="Location";
+        }
+        if ((id % 5 == 0)&&(flag)){
+            flag = false;
+           pcb.setPriority(4);
+           this.processName="Telemetry";
+        }
+        if ((id % 3 == 0)&&(flag)){
+            flag = false;
+           pcb.setPriority(5);
+           this.processName="Beacon Radio";
+        }
+        if ((id % 2 == 0)&&(flag)){
+            flag = false;
+           pcb.setPriority(6);
+           this.processName="System Health";
+        }
+        if ((id % 107==0) || (id % 113==0) || (id % 139==0) || (id % 101==0) || (id % 127==0)){
+            this.aperiodicProcess(pcb, flag);
+        }
+        return this; 
+    }
+    
+    
+    
+    public void aperiodicProcess(PCB pcb, boolean flag){
+        pcb.setPriority(7);
+        int id = (int) ((System.currentTimeMillis() % 10000000)+1);
+        pcb.setId(id);
+        if (flag){
+           /**
+ * button call
+ */
+        }else{
+            if (id % 2 == 0){
+                pcb.setUser("Mission Control Center");
+                flag = true;
+                if ((id % 3 == 0)&&(flag)){
+                    flag = false;
+                    this.processName="Update Software";
+                    pcb.setPriority(4);
+                }
+                if (flag){
+                    flag = false;
+                    this.processName="Message";
+                    pcb.setPriority(4);
+                }
+            }else{
+                flag = true;
+                pcb.setUser("Emergency");
+                if ((id % 3 == 0)&&(flag)){
+                    flag = false;
+                    this.processName="Evasion Basura temporal";
+                    pcb.setPriority(7);
+                }
+                if ((id % 5 == 0)&&(flag)){
+                    flag = false;
+                    this.processName="Full Memory";
+                    pcb.setPriority(7);
+                    
+                }
+                if ((id % 7 == 0)&&(flag)){
+                    flag = false;
+                    this.processName="Radiation Sensor";
+                    pcb.setPriority(7);
+                }
+                if (flag){
+                    flag = false;
+                    this.processName="Charging Cut";
+                    pcb.setPriority(7);
+                }
+            }
+        } 
+    }
 }
