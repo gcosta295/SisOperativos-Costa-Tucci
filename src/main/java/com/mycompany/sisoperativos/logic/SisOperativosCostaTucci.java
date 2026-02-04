0104736370bf3230b77277dda320d397aad4d787
gcosta295 <156736652+gcosta295@users.noreply.github.com>
2026-02-03 21:33:41 -0400

    Creacion de paquete logico y de interfaz
    Separar estructuras y frames en disitntos paquetes e inicializar el primer frame, mostrando el reloj y la duracion de los ciclos

--- src/main/java/com/mycompany/sisoperativos/logic/SisOperativosCostaTucci.java
+++ src/main/java/com/mycompany/sisoperativos/logic/SisOperativosCostaTucci.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.sisoperativos;
+package com.mycompany.sisoperativos.logic;
 
 /**
  *
@@ -10,14 +10,14 @@ package com.mycompany.sisoperativos;
  */
 public class SisOperativosCostaTucci {
 
-   public static void main(String[] args) throws InterruptedException {
+   public static void main(String[] args) throws InterruptedException { //Pruebas de reloj
         // 1. Iniciamos el reloj con ciclos de 1 segundo (1000ms)
         Clock miReloj = new Clock(1000);
         Thread hiloReloj = new Thread(miReloj);
         hiloReloj.start();
 
         // 2. Simulamos que después de 5 segundos, el usuario acelera el reloj
-        Thread.sleep(5000); 
+        Thread.sleep(5000); //Esto hace que no corra mas del codigo hasta que pasen los 5000 
         System.out.println(">>> Acelerando simulación a ciclos de 100ms...");
         miReloj.setDuracionCiclo(100); // Ahora el contador subirá mucho más rápido
 
